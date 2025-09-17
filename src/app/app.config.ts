b8b98862aa950a7aa2f9452b0f6d67fbded1e736
AmanGupta1542 <amangupta1542@gmail.com>
2025-09-17 17:49:14 +0530

    Successfully added tailwind css and sakai primeng template

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,22 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-  ],
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+        provideHttpClient(withFetch()),
+        provideAnimationsAsync(),
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+    ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
+// import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+// import { NxWelcome } from './nx-welcome';
+
+// @Component({
+//   imports: [NxWelcome, RouterModule],
+//   selector: 'app-root',
+//   templateUrl: './app.html',
+//   styleUrl: './app.scss',
+// })
+// export class App {
+//   protected title = 'expense_tracking';
+// }
+
+
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterModule],
+    template: `<router-outlet></router-outlet>`
 })
-export class App {
-  protected title = 'expense_tracking';
-}
+export class AppComponent {}
